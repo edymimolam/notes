b912347f4cc3cc96e0d9955e99f6f2e4efd9eb08
edymimolam <edymimolam@gmail.com>
2020-06-25 16:10:55 +0300

    create BaseIcon

---
+++ src/components/base/BaseIcon.vue
@@ -0,0 +1,22 @@
+<template>
+  <FontAwesomeIcon :icon="name" fixed-width />
+</template>
+
+<script>
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { faTimes } from "@fortawesome/free-solid-svg-icons";
+
+library.add(faSearch, faTimes);
+
+export default {
+  components: { FontAwesomeIcon },
+  props: {
+    name: {
+      type: String,
+      required: true
+    }
+  }
+};
+</script>
