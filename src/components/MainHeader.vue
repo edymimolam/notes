9b64d47caad388e7659f2de13cdaaf9aa48488d3
edymimolam <edymimolam@gmail.com>
2020-06-25 04:56:35 +0300

    basic header layout

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -1,13 +1,39 @@
 <template>
-  <header>
-    <span>Header</span>
-  </header>
+  <div class="wrapper">
+    <header class="header">
+      <div class="container">
+        <Search />
+      </div>
+    </header>
+    <div class="extender"></div>
+  </div>
 </template>
 
 <script>
+import Search from "./Search";
 export default {
-  name: "MainHeader"
+  name: "MainHeader",
+  components: { Search }
 };
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+@import "~@/design.scss";
+
+.header {
+  position: fixed;
+  top: 0;
+  width: 100%;
+  background-color: $color-bg-dark;
+  box-shadow: $shadow-header;
+}
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: $header-height;
+}
+.extender {
+  height: $header-height;
+}
+</style>
