78cf359c94a8ff6a184952dfacef244be28bc2f6
edymimolam <edymimolam@gmail.com>
2020-06-25 17:46:57 +0300

    create BaseButton

---
+++ src/components/base/BaseButton.vue
@@ -0,0 +1,25 @@
+<template>
+  <button class="button" v-on="$listeners">
+    <slot />
+  </button>
+</template>
+
+<style lang="scss" scoped>
+@import "~@/design.scss";
+
+.button {
+  padding: 10px 7.75px;
+  font-size: 20px;
+  color: $color-text-primary;
+  line-height: 20px;
+  background: none;
+  border: none;
+  border-radius: 50%;
+  cursor: pointer;
+  outline: none;
+  transition: $transition-fast;
+}
+.button:hover {
+  background-color: $color-bg-air;
+}
+</style>
