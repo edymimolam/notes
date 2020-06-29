8ae5fdee9a30d3f95b511689a54cbbcad6cd62d6
edymimolam <edymimolam@gmail.com>
2020-06-29 19:49:18 +0300

    Create BaseInput component

---
+++ src/components/base/BaseInput.vue
@@ -0,0 +1,12 @@
+<template>
+  <input class="input" v-on="$listeners" />
+</template>
+
+<style lang="scss" scoped>
+@import "~@/design.scss";
+.input {
+  padding: $size-input-padding;
+  width: 100%;
+  border: none;
+}
+</style>
